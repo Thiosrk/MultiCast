33545ddc6c2ec9b6c1352023ad0936ae08b1fcdf
Thiosrk <141250162@smail.nju.edu.cn>
2018-11-25 18:10:29 +0800

    init commit

---
+++ src/main/java/com/MultiCast/MultiCastApplication.java
@@ -0,0 +1,14 @@
+package com.MultiCast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiCastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiCastApplication.class, args);
+    }
+
+
+}
